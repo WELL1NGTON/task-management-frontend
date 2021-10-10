c26e48986f07232280a6033db4383ac8fad8d2bd
WELL1NGTON <WELL1NGTON@users.noreply.github.com>
2021-10-10 10:19:57 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.86a3a9158aca732ffd68450eed824867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e35b4584565bfc1c3bd864b34f86392",
+    "url": "https://well1ngton.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "24f57dc0e86b539762a0",
+    "url": "https://well1ngton.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "2b4fdbbe183b2829c605",
+    "url": "https://well1ngton.github.io/task-management-frontend/static/js/2.55d13524.chunk.js"
+  },
+  {
+    "revision": "24f57dc0e86b539762a0",
+    "url": "https://well1ngton.github.io/task-management-frontend/static/js/main.47da4061.chunk.js"
+  },
+  {
+    "revision": "a027765949b90539f7d5",
+    "url": "https://well1ngton.github.io/task-management-frontend/static/js/runtime~main.f3972ade.js"
+  }
+]);
